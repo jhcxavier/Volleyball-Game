6ad7a3484cc0abf39cbfdf56a3cefbcc96af2d5d
João Henrique Xavier <jhcxavier@gmail.com>
2021-02-07 14:32:00 -0800

    fully working

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+# print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 print("Volleyball Project\nJoão H Carneiro Xavier\n\n")
 team_1 = []
@@ -24,15 +24,32 @@ for i in range(1, 6):
         result_1 = score_1 - score_2
         result_2 = score_2 - score_1
         if score_1 < 15 and score_2 < 15:
-            print("Illegal score, one team must score at least 15 points. Try again.")
+            print("Illegal score, one team must score at least 15 points. Try again.\n")
         elif (result_1 >= 0) and (result_1 < 2) or (result_2 < 2) and (result_2 >= 0):
-            print("Sorry, one team must win by at least 2 points. Try again.")
-        elif (score_1 > 15 and score_2 < 14) or (score_2 > 15 and score_1 < 14):
-            print("Illegal score, you have to win by 2 when scoring over 15 points. Try again.")
+            print("Sorry, one team must win by at least 2 points. Try again.\n")
+        elif (score_1 > 15 and result_1 > 2) or (score_2 > 15 and result_2 > 2):
+            print("Illegal score, you have to win by 2 when scoring over 15 points. Try again.\n")
         else:
             team_1 = [*team_1, score_1]
             team_2 = [*team_2, score_2]
             game_changer = True
     print('\n')
-print(f"team 1 score: {team_1}")
-print(f"team 2 score: {team_2}")
+print("{:>5s}{:>10s}{:>10s}{:>10s}".format("Game", "Team 1", "Team 2", "Winner"))
+
+team_winner_1 = 0
+team_winner_2 = 0
+winner = ""
+for i in range(0, 5):
+    if team_1[i] > team_2[i]:
+        winner = "Team 1"
+        team_winner_1 += 1
+    else:
+        winner = "Team 2"
+        team_winner_2 += 1
+    # print("printList", team_1[i-1])
+    print("{:>5d}{:>10d}{:>10d}{:>10s}".format(i+1, team_1[i], team_2[i], winner))
+
+if team_winner_1 > team_winner_2:
+    print("\nThe winner of the match is Team 1")
+else:
+    print("\nThe winner of the match is Team 2")
